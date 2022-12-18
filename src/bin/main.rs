ca1d9c552f42046fdc744b22f848cbd917784b38
Robin Lake <robin@tempus-ex.com>
2022-12-18 13:07:33 -0800

    refactor

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -9,10 +9,9 @@ use rust_server::ThreadPool;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
-
     let pool = ThreadPool::new(4);
 
-    for stream in listener.incoming() {
+    for stream in listener.incoming().take(2) {
         let stream = stream.unwrap();
         pool.execute(|| {
             handle_connection(stream);
@@ -22,8 +21,6 @@ fn main() {
 
 fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
-
-
     stream.read(&mut buffer).unwrap();
 
     let get = b"GET / HTTP/1.1\r\n";
